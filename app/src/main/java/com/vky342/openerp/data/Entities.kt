7f60148f6afca08dddb65ba24c21e7b6664acebe
kunalsahu <kunalsahu071@gmail.com>
2024-07-06 20:22:02 +0530

    Created Entities.kt

---
+++ app/src/main/java/com/vky342/openerp/data/Entities.kt
@@ -0,0 +1,59 @@
+package com.vky342.openerp.data
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Accounts")
+data class Account(
+    @PrimaryKey
+    val name : String,
+    val address : String,
+    val contact : Int,
+    )
+
+@Entity(
+    tableName = "BILLS",
+    foreignKeys = [
+        ForeignKey(entity = Account::class, parentColumns = ["name"], childColumns = ["accountNameFk"])
+    ]
+)
+data class Bill(
+    @PrimaryKey
+    val billId : Int,
+    val billDate : Int,
+    val billAmount : Int = 0,
+    val billType : Int, /// either sale(1) or purchase(0)
+
+//foreign keys
+    val accountNameFk : String
+
+)
+
+@Entity(
+    tableName = "BillEntries",
+    primaryKeys = ["entryId"],
+    foreignKeys = [
+        ForeignKey(entity = Bill::class, parentColumns = ["BillId"], childColumns = ["BillIdFk"], onDelete = ForeignKey.CASCADE, onUpdate = ForeignKey.CASCADE),
+        ForeignKey(entity = ItemInventory::class, parentColumns = ["itemName"], childColumns = ["itemNameFk"], onDelete = ForeignKey.RESTRICT, onUpdate = ForeignKey.CASCADE)
+    ])
+data class BillEntries(
+    val entryId : Int,
+    val entryQuantity : Int,
+    val discount : Int = 0,
+    val finalPrice : Int,
+
+    //Foreign keys
+
+    val billIdFk : Int,
+    val itemNameFk : String,
+)
+
+@Entity(tableName = "ItemInventory")
+data class ItemInventory(
+    @PrimaryKey
+    val itemName : String,
+    val itemListPrice : Int,
+    val itemPurchasePrice : Int,
+    val itemQuantity : Int,
+)
