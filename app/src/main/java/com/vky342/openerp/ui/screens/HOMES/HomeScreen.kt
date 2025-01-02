915ddc3720e1b69058cebd182a0dadf6948a345f
kunalsahu <kunalsahu071@gmail.com>
2025-01-02 23:24:05 +0530

    simple projects gradle update

--- app/src/main/java/com/vky342/openerp/ui/screens/HOMES/HomeScreen.kt
+++ app/src/main/java/com/vky342/openerp/ui/screens/HOMES/HomeScreen.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
