7f3b1d13c59bba6b9d5253b9b287ab60ec4e18d2
kunalsahu <kunalsahu071@gmail.com>
2025-01-03 00:21:01 +0530

    simple projects gradle update

--- app/src/main/java/com/vky342/openerp/ui/screens/RootScreen.kt
+++ app/src/main/java/com/vky342/openerp/ui/screens/RootScreen.kt
@@ -12,9 +12,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.outlined.Edit
@@ -43,15 +48,16 @@ import com.vky342.openerp.ui.Graphs.Graph
 import com.vky342.openerp.ui.Graphs.RootNavigationGraph
 import com.vky342.openerp.ui.theme.Greye
 
-@Preview
+
 @Composable
 fun RootScreen(navController: NavHostController = rememberNavController()){
-    Scaffold (topBar = { topAppBar() }, containerColor = Greye,
+    Scaffold (topBar = { TopAppBar() }, containerColor = Color.White,
         bottomBar = { BottomBar(navController = navController) }
     ){
         RootNavigationGraph(navController = navController, paddingValues = it)
     }
 }
+
 
 @Composable
 fun BottomBar(navController: NavHostController){
@@ -99,7 +105,7 @@ fun BottomBar(navController: NavHostController){
 
     colorFilling()
 
-    BottomAppBar(modifier = Modifier.height(topPadding.dp), containerColor = Color.LightGray){
+    BottomAppBar(modifier = Modifier.height(topPadding.dp), containerColor = Color.White){
         Box(modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp), contentAlignment = Alignment.Center){
 
             Row (modifier = Modifier.wrapContentSize()){
@@ -154,6 +160,30 @@ fun BottomBar(navController: NavHostController){
 
                 Column (modifier = Modifier.fillMaxHeight().weight(1f)){
 
+                    Box(modifier = Modifier
+                        .wrapContentSize()
+                        .align(Alignment.CenterHorizontally)){
+
+                        Column(modifier = Modifier.fillMaxHeight().align(Alignment.Center)) {
+                            Icon(imageVector = Ledger, tint = ledgerColor, contentDescription = "ADD Sale", modifier = Modifier
+                                .size(IconHeight.dp)
+                                .align(Alignment.CenterHorizontally)
+                                .clickable(onClick = {
+                                    navController.navigate(Graph.LEDGER) {
+                                        popUpTo(navController.graph.findStartDestination().id)
+                                        launchSingleTop = true
+                                    }
+                                }))
+
+                            Text(text = "Ledger", color = Color.Black, fontSize = 15.sp, fontWeight = FontWeight(700), modifier = Modifier.align(Alignment.CenterHorizontally))
+                        }
+
+                    }
+
+                }
+
+                Column (modifier = Modifier.fillMaxHeight().weight(1f)){
+
 
                     Box(modifier = Modifier
                         .wrapContentSize()
@@ -178,29 +208,7 @@ fun BottomBar(navController: NavHostController){
                     }
                 }
 
-                Column (modifier = Modifier.fillMaxHeight().weight(1f)){
 
-                    Box(modifier = Modifier
-                        .wrapContentSize()
-                        .align(Alignment.CenterHorizontally)){
-
-                        Column(modifier = Modifier.fillMaxHeight().align(Alignment.Center)) {
-                            Icon(imageVector = Ledger, tint = ledgerColor, contentDescription = "ADD Sale", modifier = Modifier
-                                .size(IconHeight.dp)
-                                .align(Alignment.CenterHorizontally)
-                                .clickable(onClick = {
-                                    navController.navigate(Graph.LEDGER) {
-                                        popUpTo(navController.graph.findStartDestination().id)
-                                        launchSingleTop = true
-                                    }
-                                }))
-
-                            Text(text = "Ledger", color = Color.Black, fontSize = 15.sp, fontWeight = FontWeight(700), modifier = Modifier.align(Alignment.CenterHorizontally))
-                        }
-
-                    }
-
-                }
 
             }
 
@@ -213,17 +221,170 @@ fun BottomBar(navController: NavHostController){
 
 @Preview
 @Composable
-fun topAppBar(){
+fun TopAppBar(){
     val height = LocalConfiguration.current.run { screenHeightDp.dp}
+    val sidePadding = 20.dp
     val topPadding = height.value * 0.1
     val statusBar = topPadding * 0.3
     Box(modifier = Modifier
         .fillMaxWidth()
-        .background(color = Color.LightGray)
+        .background(color = Color.White)
         .height(topPadding.dp)){
         Text(fontSize = 34.sp, color = Greye, fontWeight = FontWeight(500),text = "OpenERP", modifier = Modifier
-            .align(Alignment.Center)
+            .align(Alignment.CenterStart)
+            .padding(horizontal = sidePadding)
             .padding(top = statusBar.dp))
+
+        Box(modifier = Modifier
+            .wrapContentWidth()
+            .height(topPadding.dp)
+            .background(color = Color.White)
+            .align(Alignment.CenterEnd)
+            .padding(horizontal = sidePadding)
+            ){
+
+            Row (
+                modifier = Modifier.wrapContentWidth().align(Alignment.Center),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Default.Notifications, contentDescription = "",
+                    modifier = Modifier
+                        .padding(horizontal = 4.dp)
+                        .padding(top = statusBar.dp))
+
+
+
+                Icon(imageVector = Icons.Default.DateRange, contentDescription = "",
+                    modifier = Modifier
+                        .padding(horizontal = 4.dp)
+                        .padding(top = statusBar.dp))
+
+
+
+                Icon(imageVector = Icons.Default.Menu, contentDescription = "",
+                    modifier = Modifier
+                        .padding(horizontal = 4.dp)
+                        .padding(top = statusBar.dp))
+            }
+
+        }
+
+
     }
 
+}
+
+@Preview
+@Composable
+fun bottombar_test() {
+
+    val height = LocalConfiguration.current.run { screenHeightDp.dp }
+    val topPadding = height.value * 0.1
+    val IconHeight = topPadding/3
+
+    var HomeIcon = Icons.Outlined.Home
+    var HomeIconColor = Color.Black
+
+    var AccountIcon = Icons.Outlined.Person
+    var AccountColor = Color.Black
+
+    var Transicon = Icons.Outlined.ShoppingCart
+    var TransColor = Color.Black
+
+    var Ledger = Icons.Outlined.Edit
+    var ledgerColor = Color.Black
+//
+//    val navBackStackEntry by navController.currentBackStackEntryAsState()
+//    val currentDestination = navBackStackEntry?.destination
+//
+    BottomAppBar(modifier = Modifier.height(topPadding.dp), containerColor = Color.White){
+        Box(modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp), contentAlignment = Alignment.Center){
+
+            Row (modifier = Modifier.wrapContentSize()){
+
+                Column(modifier = Modifier.fillMaxHeight().weight(1f)) {
+
+                    Box(modifier = Modifier
+                        .wrapContentSize()
+                        .align(Alignment.CenterHorizontally)){
+
+                        Column(modifier = Modifier.fillMaxHeight().align(Alignment.Center)) {
+                            Icon(imageVector = HomeIcon, tint = HomeIconColor, contentDescription = "home", modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .size(IconHeight.dp)
+                                )
+
+                            Text(text = "Home", color = Color.Black, fontSize = 15.sp, fontWeight = FontWeight(weight = 700), modifier = Modifier.align(Alignment.CenterHorizontally))
+                        }
+                    }
+
+                }
+
+                Column (modifier = Modifier.fillMaxHeight().weight(1f)){
+
+                    Box(modifier = Modifier
+                        .wrapContentSize()
+                        .align(Alignment.CenterHorizontally)){
+
+                        Column(modifier = Modifier.fillMaxHeight().align(Alignment.Center)) {
+
+                            Icon(imageVector = AccountIcon, tint = AccountColor, contentDescription = "Account", modifier = Modifier
+                                .size(IconHeight.dp)
+                                .align(Alignment.CenterHorizontally)
+                                )
+                            Text(text = "Account", color = Color.Black, fontSize = 15.sp, fontWeight = FontWeight(700), modifier = Modifier.align(Alignment.CenterHorizontally))
+
+                        }
+                    }
+
+                }
+
+                Column (modifier = Modifier.fillMaxHeight().weight(1f)){
+
+                    Box(modifier = Modifier
+                        .wrapContentSize()
+                        .align(Alignment.CenterHorizontally)){
+
+                        Column(modifier = Modifier.fillMaxHeight().align(Alignment.Center)) {
+                            Icon(imageVector = Ledger, tint = ledgerColor, contentDescription = "ADD Sale", modifier = Modifier
+                                .size(IconHeight.dp)
+                                .align(Alignment.CenterHorizontally)
+                            )
+
+                            Text(text = "Ledger", color = Color.Black, fontSize = 15.sp, fontWeight = FontWeight(700), modifier = Modifier.align(Alignment.CenterHorizontally))
+                        }
+
+                    }
+
+                }
+
+                Column (modifier = Modifier.fillMaxHeight().weight(1f)){
+
+
+                    Box(modifier = Modifier
+                        .wrapContentSize()
+                        .align(Alignment.CenterHorizontally)){
+
+                        Column(modifier = Modifier.fillMaxHeight().align(Alignment.Center)) {
+                            Icon(imageVector = Transicon, tint = TransColor, contentDescription = "ADD Sale", modifier = Modifier
+                                .size(IconHeight.dp)
+                                .align(Alignment.CenterHorizontally)
+                                )
+
+                            Text(text = "Bills", color = Color.Black, fontSize = 15.sp, fontWeight = FontWeight(700), modifier = Modifier.align(Alignment.CenterHorizontally))
+
+
+                        }
+                    }
+                }
+
+
+
+            }
+
+
+        }
+
+
+    }
 }
